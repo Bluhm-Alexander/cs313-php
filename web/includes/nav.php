44c47ae4647220843035679ee3ac382ab71b16ce
Retlekent Equate <alexbluhm1993@gmail.com>
2019-09-25 18:01:54 -0500

    small fixes 1

--- web/includes/nav.php
+++ web/includes/nav.php
@@ -1,11 +1,11 @@
 <a href="index.php">Home</a>
 <p>Assignments</p>
 <ul>
-	<li>Week01</li>
+	<li>Week01:</li>
 		<ul>
 			<li><a href="hello.php">Prove01: Hello World</a></li>
 		</ul>
-	<li>Week02</li>
+	<li>Week02:</li>
 		<ul>
 			<li><a href="02teach.php">Teach02: Team Activity</a></li>
 			<li><a href="02prove.php">Prove02: Homepage</a></li>
