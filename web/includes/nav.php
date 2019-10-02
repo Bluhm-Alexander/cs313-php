9c6298627e12d61aab1db39eb69e775af2d15500
Retlekent Equate <alexbluhm1993@gmail.com>
2019-10-02 14:12:51 -0500

    Added links to week03

--- web/includes/nav.php
+++ web/includes/nav.php
@@ -18,4 +18,8 @@
 			<li><a href="<?php echo $upOne; ?>week02/02teach.php">Teach02: Team Activity</a></li>
 			<li><a href="<?php echo $upOne; ?>index.php">Prove02: Homepage</a></li>
 		</ul>
+	<li>Week03:</li>
+		<ul>
+			<li><a href="<?php echo $upOne; ?>week03/03teach.php">Teach03: Team Activity</a></li>
+		</ul>
 </ul>
